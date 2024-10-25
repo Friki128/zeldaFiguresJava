a8fc1383fdccdf97744716e42f9d77007d5ee78b
Zelda <zeldagarrocho@gmail.com>
2024-10-25 16:50:28 +0200

    delete version controller

---
+++ java/Controlers/deleteVersionController.java
@@ -0,0 +1,31 @@
+package Controlers;
+
+import Exceptions.notOwnerException;
+import Services.drawingService;
+import Model.user;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/deleteVersion")
+public class deleteVersionController extends HttpServlet {
+    drawingService drawingService = new drawingService();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int drawingId = Integer.parseInt(req.getParameter("drawingId"));
+        int versionId = Integer.parseInt(req.getParameter("versionId"));
+        user user = (Model.user) req.getSession().getAttribute("user");
+        try {
+            drawingService.removeVersion(drawingId, user, versionId);
+        } catch (notOwnerException e) {
+            req.setAttribute("error", "Cannot remove a version of a drawing you don't own");
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/errorPage.jsp");
+            requestDispatcher.forward(req, resp);
+        }
+    }
+}
