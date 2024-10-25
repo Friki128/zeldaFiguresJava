53ec7b469f7505dacec74cafa46f1cb4cbd0455e
Zelda <zeldagarrocho@gmail.com>
2024-10-25 15:54:15 +0200

    login controller

---
+++ java/Controlers/LoginController.java
@@ -0,0 +1,41 @@
+package Controlers;
+
+import Model.user;
+import Services.userService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/login")
+public class LoginController extends HttpServlet {
+    userService userService = new userService();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getSession().getAttribute("user") != null){
+            resp.sendRedirect("/");
+        }else{
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/login.jsp");
+            requestDispatcher.forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        user user = userService.login(name, password);
+        if(user != null){
+            req.getSession().setAttribute("client", user);
+            resp.sendRedirect("/");
+        }else{
+            req.setAttribute("error", "Name and/or Password incorrect. Could not login.");
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/login.jsp");
+            requestDispatcher.forward(req, resp);
+        }
+    }
+}
