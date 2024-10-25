69d3cc9e070fee8ac0d9db63824ccc50394d9239
Zelda <zeldagarrocho@gmail.com>
2024-10-25 16:55:30 +0200

    update Drawing Name controller

---
+++ java/Controlers/updateDrawingNameController.java
@@ -0,0 +1,32 @@
+package Controlers;
+
+import Exceptions.notOwnerException;
+import Model.user;
+import Services.drawingService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/updateDrawingName")
+public class updateDrawingNameController extends HttpServlet {
+    drawingService drawingService = new drawingService();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("drawingId"));
+        String name = req.getParameter("name");
+        user user = (Model.user) req.getSession().getAttribute("user");
+        try {
+            drawingService.changeDrawingName(id, user, name);
+        } catch (notOwnerException e) {
+            req.setAttribute("error", "Can't change drawing name without being the owner.");
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/errorPage.jsp");
+            requestDispatcher.forward(req, resp);
+        }
+
+    }
+}
