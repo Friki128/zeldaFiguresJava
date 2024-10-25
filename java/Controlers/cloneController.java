75447dfb674905f6f563466623c19a88924df88e
Zelda <zeldagarrocho@gmail.com>
2024-10-25 19:48:48 +0200

    clone controller

---
+++ java/Controlers/cloneController.java
@@ -0,0 +1,29 @@
+package Controlers;
+
+import Exceptions.notPublicException;
+import Model.user;
+import Services.drawingService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/clone")
+public class cloneController extends HttpServlet {
+    drawingService drawingService = new drawingService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        user user = (Model.user) req.getSession().getAttribute("user");
+        int id = Integer.parseInt(req.getParameter("id"));
+        try {
+            drawingService.duplicate(id, user);
+            resp.sendRedirect("/viewUserDrawings");
+        } catch (notPublicException e) {
+            errorController.redirectError("Cannot clone a drawing that isn't public", req, resp);
+        }
+    }
+}
